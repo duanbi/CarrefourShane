64f82dfa36bbfbd944796777a28b8da1675bc2c5
duanbi <ruifeng_mao@hotmail.com>
2019-03-07 10:09:55 +0800

    搭建项目框架

---
+++ CarrefourShane.Entities/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CarrefourShane.Entities
+{
+    public class Class1
+    {
+    }
+}
---
+++ CarrefourShane.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CarrefourShane.Service
+{
+    public class Class1
+    {
+    }
+}
