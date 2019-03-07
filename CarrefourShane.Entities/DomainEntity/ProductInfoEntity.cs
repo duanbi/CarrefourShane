1a6f7829520f424c5c8f831d01b37cb3367d1eea
duanbi <ruifeng_mao@hotmail.com>
2019-03-07 10:29:23 +0800

    提交product service 和 entity

---
+++ CarrefourShane.Entities/DomainEntity/ProductInfoEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarrefourShane.Entities.DomainEntity
+{
+    public class ProductInfoEntity
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string ProductNumber { get; set; }
+    }
+}
---
+++ CarrefourShane.Service/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarrefourShane.Service
+{
+    public interface IProductService
+    {
+    }
+}
