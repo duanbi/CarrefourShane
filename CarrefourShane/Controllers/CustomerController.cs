2b66ef05d5f37b298a8830d7d40a3b25d5734744
duanbi <ruifeng_mao@hotmail.com>
2019-03-07 10:49:59 +0800

    Customer 基础搭建

---
+++ CarrefourShane/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarrefourShane.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarrefourShane.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private ICustomerService _customerService;
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+    }
+}
